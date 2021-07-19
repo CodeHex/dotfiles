bbc54b64eae75da324304fbd0e0c37f5db60cbcc
Ben Ibinson <ben@ibinson.dev>
2021-07-19 20:06:58 +0100

    Update mac settings

--- functions.zsh
+++ functions.zsh
@@ -91,8 +91,27 @@ function update_homebrew_bundle {
 function configure_mac {
 	log_ok "⚙️  Setting up Mac system settings..."
 
+	# Finder: show hidden files by default
+	defaults write com.apple.finder AppleShowAllFiles -bool true
+	
+	# Save screenshots to the desktop
+	defaults write com.apple.screencapture location -string "$HOME/Desktop"
+	
+	# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+	defaults write com.apple.screencapture type -string "png"
+	
+	# Display full POSIX path as Finder window title
+	defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+	# Keep folders on top when sorting by name
+	defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+	# Finder: show all filename extensions
+	defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+	
 	# Reduce the size of the dock icons
-	defaults write com.apple.dock tilesize -int 48
+	defaults write com.apple.dock tilesize -int 42
+
 	# Don't show recent apps
 	defaults write com.apple.dock show-recents -bool FALSE
 	killall Dock
@@ -100,6 +119,13 @@ function configure_mac {
 	# Show hidden files
 	defaults write com.apple.finder AppleShowAllFiles TRUE
 	killall Finder
+
+	# Show the ~/Library folder
+	chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+	
+	# Don’t display the annoying prompt when quitting iTerm
+	defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+	
 	log_info " - settings applied"
 }
 
